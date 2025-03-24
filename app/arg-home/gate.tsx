70bfe7cab212fcba1fb3d4fb8d5fc51255c7295f
AveryBigWhale <artfestarg@gmail.com>
2025-03-24 15:40:29 +0800

    change the path to absolute for .png -3

--- app/arg-home/gate.tsx
+++ app/arg-home/gate.tsx
@@ -137,8 +137,8 @@ export default function Page() {
   // 計算背景圖片的實際尺寸和位置
   useEffect(() => {
     const img = new window.Image();
-    // img.src = '/puzzle-bg.png';
-    img.src = 'https://averybigwhale.github.io/entry/public/puzzle-bg.png';
+    img.src = '/puzzle-bg.png';
+    // img.src = 'https://averybigwhale.github.io/entry/public/puzzle-bg.png';
     img.onload = () => {
       const imgRatio = img.width / img.height;
       const windowRatio = windowSize.width / windowSize.height;
@@ -239,8 +239,8 @@ export default function Page() {
             position: 'absolute',
             width: '100%',
             height: '100%',
-            backgroundImage: "url('https://averybigwhale.github.io/entry/public/puzzle-bg.png')",
-            // backgroundImage: "url('/puzzle-bg.png')",
+            // backgroundImage: "url('https://averybigwhale.github.io/entry/public/puzzle-bg.png')",
+            backgroundImage: "url('/puzzle-bg.png')",
             backgroundSize: 'cover',
             // backgroundSize: `${windowSize.width}px ${windowSize.height}px`,
             backgroundPosition: 'center',
@@ -318,8 +318,8 @@ export default function Page() {
           top: `${holePosition.y}px`,
           width: '100px',
           height: '100px',
-          backgroundImage: "url('https://averybigwhale.github.io/entry/public/puzzle-bg.png')",
-          // backgroundImage: "url('/puzzle-bg.png')",
+          // backgroundImage: "url('https://averybigwhale.github.io/entry/public/puzzle-bg.png')",
+          backgroundImage: "url('/puzzle-bg.png')",
           backgroundSize: `${backgroundSize.width}px ${backgroundSize.height}px`,
           backgroundPosition: `${-holePosition.x + backgroundPosition.x}px ${-holePosition.y + backgroundPosition.y}px`,
           clipPath: PUZZLE_SHAPE_PIXELS,
@@ -342,8 +342,8 @@ export default function Page() {
           top: `${holePosition.y}px`,
           width: '100px',
           height: '100px',
-          backgroundImage: "url('https://averybigwhale.github.io/entry/public/puzzle-bg.png')",
-          // backgroundImage: "url('/puzzle-bg.png')",
+          // backgroundImage: "url('https://averybigwhale.github.io/entry/public/puzzle-bg.png')",
+          backgroundImage: "url('/puzzle-bg.png')",
           backgroundSize: `${backgroundSize.width}px ${backgroundSize.height}px`,
           backgroundPosition: `${-holePosition.x + backgroundPosition.x}px ${-holePosition.y + backgroundPosition.y}px`,
           opacity: isOverDropZone && !isCompleted ? 0.5 : 0,

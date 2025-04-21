aaace131661c618e2f0e53bc5d713dc8ad691857
Koikbin <kaoming777@gmail.com>
2025-04-22 01:40:06 +0800

    add titles

--- app/arg-home/layout.tsx
+++ app/arg-home/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import React from 'react';
-import Script from 'next/script';
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,29 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     //   {children}  {/* Render the children (pages/components inside this layout) */}
     // </div>
     <html lang="zh-TW">
-      <head>
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-FZCK9PB2KG"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-FZCK9PB2KG');
-          `}
-        </Script>
-        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-FZCK9PB2KG"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-FZCK9PB2KG');
-        </script> */}
-      </head>
+      
       <body>{children}</body>
     </html>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,31 @@
+import Script from 'next/script';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FZCK9PB2KG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FZCK9PB2KG');
+          `}
+        </Script>
+        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-FZCK9PB2KG"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FZCK9PB2KG');
+        </script> */}
+      </head>
       <body style={{ margin: 0, padding: 0 }}>
         {children}
       </body>

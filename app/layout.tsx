0334204da3629e6675bf7d83a046a4d8a29bd309
Koikbin <kaoming777@gmail.com>
2025-04-27 18:17:59 +0800

    revive google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+// import Script from 'next/script';
+// import { GoogleAnalytics } from '@next/third-parties/google'
 import Script from 'next/script';
-
+ 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-FZCK9PB2KG"
+          src="https://www.googletagmanager.com/gtag/js?id=G-FP01S99JY0"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -14,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-FZCK9PB2KG');
+            gtag('config', 'G-FP01S99JY0');
           `}
         </Script>
         {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-FZCK9PB2KG"></script>
